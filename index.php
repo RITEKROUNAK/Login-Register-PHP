50df4bc82eae0b5b1941f1cbee93b280be715f59
RITEKROUNAK <ritekrounak1@gmail.com>
2022-07-28 02:19:44 +0530

    :closed_lock_with_key: Login Function

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+error_reporting(0);
+
+if (isset($_SESSION['username'])) {
+    header("Location: welcome.php");
+}
+
+if (isset($_POST['submit'])) {
+    $email = $_POST['email'];
+    $password = md5($_POST['password']);
+
+    $sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+    $result = mysqli_query($conn, $sql);
+    if ($result->num_rows > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['username'] = $row['username'];
+        header("Location: welcome.php");
+    } else {
+        echo "<script>alert('Woops! Email or Password is Wrong.')</script>";
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
